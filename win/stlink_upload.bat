9d655e1880145895719a2a71e686cc3800ee203c
Frederic Pillon <frederic.pillon@st.com>
2018-10-08 11:04:27 +0200

    Clean old and unused files
    Signed-off-by: Frederic Pillon <frederic.pillon@st.com>

--- win/stlink_upload.bat
+++ win/stlink_upload.bat
@@ -14,6 +14,3 @@ rem: ------------- use STLINK CLI
 rem: echo "" is mandatory to exit ST-Link_CLI tools w 3.0.0.0 when "-Run" option is used,
 rem: it now wait Enter key press to quit.
 echo "" | stlink\ST-LINK_CLI.exe -c SWD -P %str% 0x8000000 -Rst -Run
-
-rem: Using the open source texane-stlink instead of the proprietary STM stlink exe
-rem:texane-stlink\st-flash.exe write %str% 0x8000000
