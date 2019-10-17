775cb473bd5b57543e7ebd5be10b32de721cef25
Frederic Pillon <frederic.pillon@st.com>
2019-10-17 15:57:07 +0200

    [STM32CubeProg] Wait for serial port availability
    Signed-off-by: Frederic Pillon <frederic.pillon@st.com>

--- linux/stm32CubeProg.sh
+++ linux/stm32CubeProg.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 set -o nounset                              # Treat unset variables as an error
 #set -x
+
+# STM32 Cube programmer variables
 STM32CP_CLI=STM32_Programmer.sh
 ADDRESS=0x8000000
 ERASE=
 MODE=
 PORT=
 OPTS=
+
+# Script variables
+SERPORT=
+STATUS=
 
 ###############################################################################
 ## Help function
@@ -24,10 +30,12 @@ usage()
   echo "##         Ex: 10 erase all sectors using SWD interface."
   echo "## file_path: file path name to be downloaded: (bin, hex)"
   echo "## Options:"
-  echo "##   For SWD and DFU: no mandatory options"
-  echo "##   For Serial: <com_port>"
+  echo "##   For SWD: no mandatory options"
+  echo "##   For DFU: no mandatory options"
+  echo "##   For Serial: 'serport=<com_port>'"
   echo "##     com_port: serial identifier (mandatory). Ex: /dev/ttyS0"
   echo "##"
+  echo "##   '-serport=<com_port>' is also used to wait the serial port availability"
   echo "## Note: all trailing arguments will be passed to the $STM32CP_CLI"
   echo "##   They have to be valid commands for STM32 MCU"
   echo "##   Ex: -g: Run the code at the specified address"
@@ -36,7 +44,6 @@ usage()
   echo "############################################################"
   exit $1
 }
-
 
 check_tool() {
   command -v $STM32CP_CLI >/dev/null 2>&1
@@ -53,6 +60,18 @@ check_tool() {
   fi
 }
 
+upload() {
+  count=0
+  STATUS=1
+  while [ $STATUS -ne 0 ] && ((count++ < 5)); do
+    # echo "Try upload $count "
+    ${STM32CP_CLI} -c port=${PORT} ${MODE} ${ERASE} -q -d ${FILEPATH} ${ADDRESS} ${OPTS}
+    STATUS=$?
+    sleep 0.5
+  done
+}
+
+# Main
 check_tool
 
 if [ $# -lt 2 ]; then
@@ -69,6 +88,15 @@ if [ $1 -ge 10 ]; then
   ERASE='-e all'
   PROTOCOL=$(($1 - 10))
 fi
+
+# Check if serial port option available
+if [ $# -gt 2 ] && [[ $3 == "-serport="* ]]; then
+  SERPORT=`echo $3 | cut -d'=' -f2`
+  if [ ! -z $SERPORT ] && [[ $SERPORT != "/dev/"* ]]; then
+    SERPORT="/dev/"${SERPORT}
+  fi
+fi
+
 # Protocol $1
 # 0: SWD
 # 1: Serial
@@ -76,28 +104,47 @@ fi
 case $PROTOCOL in
   0)
     PORT='SWD'
-    MODE='mode=UR'
-    shift 2;;
+    MODE='mode=UR';;
   1)
-    if [ $# -lt 3 ]; then
+    if [ -z $SERPORT ]; then
+      echo "Missing Serial port!"
       usage 3
-    else
-      PORT=$3
-      shift 3
-    fi;;
+    fi
+    PORT=$SERPORT;;
   2)
-    PORT='USB1'
-    shift 2;;
+    PORT='USB1';;
   *)
     echo "Protocol unknown!"
     usage 4;;
 esac
 
+if [ -z $SERPORT ]; then
+  shift 2
+else
+  shift 3
+fi
+
 if [ $# -gt 0 ]; then
   OPTS="$@"
 fi
 
-${STM32CP_CLI} -c port=${PORT} ${MODE} ${ERASE} -q -d ${FILEPATH} ${ADDRESS} ${OPTS}
+upload
 
-exit 0
+if [ ! -z $SERPORT ] && [ $STATUS -eq 0 ]; then
+  echo -n "Waiting for $SERPORT serial..."
+  count=0
+  while [ ! -c $SERPORT ] && ((count++ < 40)); do
+    sleep 0.1
+  done
+  count=0
+  res=1
+  while [ $res -ne 0 ] && ((count++ < 20)); do
+    stty -F $SERPORT > /dev/null 2>&1
+    res=$?
+    sleep 1
+  done
+  echo "done"
+fi
+
+exit $STATUS
 
